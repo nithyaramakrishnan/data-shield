7852a2004941e022fdbb1e118485d4c75572c3ce
Kristin A. Brown <kakronst@us.ibm.com>
2016-09-15 09:35:44 -0400

    Copied from dita repo

---
+++ jenkins_script/process_translations.bat
@@ -0,0 +1,26 @@
+REM de
+CALL ant -f C:\FileNet\Work\Current\IDCMS1\CPS\CloudOE\dita-translation-processing-master\handling_translated_files.xml -Dlang=de -DnoPrompt=true -DshipmentNumber=%shipmentNumber% -Dplugindir=%plugindir% -DcheckInComment="%checkInComment%" -Dgsa.userid=%gsaUserID% -Dgsa.password=%gsaUserPassword% %subdirectorySet1% %subdirectorySet2% %subdirectorySet3% %subdirectorySet4% %subdirectorySet5% -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/%projectCode%/%chargetoID%/%chargetoID%_%shipmentName%_%shipmentNumber%_de.pkg
+
+REM es
+CALL ant -f C:\FileNet\Work\Current\IDCMS1\CPS\CloudOE\dita-translation-processing-master\handling_translated_files.xml -Dlang=es -DnoPrompt=true -DshipmentNumber=%shipmentNumber% -Dplugindir=%plugindir% -DcheckInComment="%checkInComment%" -Dgsa.userid=%gsaUserID% -Dgsa.password=%gsaUserPassword% %subdirectorySet1% %subdirectorySet2% %subdirectorySet3% %subdirectorySet4% %subdirectorySet5% -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/%projectCode%/%chargetoID%/%chargetoID%_%shipmentName%_%shipmentNumber%_es.pkg
+
+REM fr
+CALL ant -f C:\FileNet\Work\Current\IDCMS1\CPS\CloudOE\dita-translation-processing-master\handling_translated_files.xml -Dlang=fr -DnoPrompt=true -DshipmentNumber=%shipmentNumber% -Dplugindir=%plugindir% -DcheckInComment="%checkInComment%" -Dgsa.userid=%gsaUserID% -Dgsa.password=%gsaUserPassword% %subdirectorySet1% %subdirectorySet2% %subdirectorySet3% %subdirectorySet4% %subdirectorySet5% -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/%projectCode%/%chargetoID%/%chargetoID%_%shipmentName%_%shipmentNumber%_fr.pkg
+
+REM it	
+CALL ant -f C:\FileNet\Work\Current\IDCMS1\CPS\CloudOE\dita-translation-processing-master\handling_translated_files.xml -Dlang=it -DnoPrompt=true -DshipmentNumber=%shipmentNumber% -Dplugindir=%plugindir% -DcheckInComment="%checkInComment%" -Dgsa.userid=%gsaUserID% -Dgsa.password=%gsaUserPassword% %subdirectorySet1% %subdirectorySet2% %subdirectorySet3% %subdirectorySet4% %subdirectorySet5% -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/%projectCode%/%chargetoID%/%chargetoID%_%shipmentName%_%shipmentNumber%_it.pkg
+
+REM ja
+CALL ant -f C:\FileNet\Work\Current\IDCMS1\CPS\CloudOE\dita-translation-processing-master\handling_translated_files.xml -Dlang=ja -DnoPrompt=true -DshipmentNumber=%shipmentNumber% -Dplugindir=%plugindir% -DcheckInComment="%checkInComment%" -Dgsa.userid=%gsaUserID% -Dgsa.password=%gsaUserPassword% %subdirectorySet1% %subdirectorySet2% %subdirectorySet3% %subdirectorySet4% %subdirectorySet5% -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/%projectCode%/%chargetoID%/%chargetoID%_%shipmentName%_%shipmentNumber%_ja.pkg
+
+REM KO
+CALL ant -f C:\FileNet\Work\Current\IDCMS1\CPS\CloudOE\dita-translation-processing-master\handling_translated_files.xml -Dlang=ko -DnoPrompt=true -DshipmentNumber=%shipmentNumber% -Dplugindir=%plugindir% -DcheckInComment="%checkInComment%" -Dgsa.userid=%gsaUserID% -Dgsa.password=%gsaUserPassword% %subdirectorySet1% %subdirectorySet2% %subdirectorySet3% %subdirectorySet4% %subdirectorySet5% -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/%projectCode%/%chargetoID%/%chargetoID%_%shipmentName%_%shipmentNumber%_ko.pkg
+
+REM PT/BR
+CALL ant -f C:\FileNet\Work\Current\IDCMS1\CPS\CloudOE\dita-translation-processing-master\handling_translated_files.xml -Dlang=pt/BR -DnoPrompt=true -DshipmentNumber=%shipmentNumber% -Dplugindir=%plugindir% -DcheckInComment="%checkInComment%" -Dgsa.userid=%gsaUserID% -Dgsa.password=%gsaUserPassword% %subdirectorySet1% %subdirectorySet2% %subdirectorySet3% %subdirectorySet4% %subdirectorySet5% -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/%projectCode%/%chargetoID%/%chargetoID%_%shipmentName%_%shipmentNumber%_pt-BR.pkg
+
+REM zh/CN
+CALL ant -f C:\FileNet\Work\Current\IDCMS1\CPS\CloudOE\dita-translation-processing-master\handling_translated_files.xml -Dlang=zh/CN -DnoPrompt=true -DshipmentNumber=%shipmentNumber% -Dplugindir=%plugindir% -DcheckInComment="%checkInComment%" -Dgsa.userid=%gsaUserID% -Dgsa.password=%gsaUserPassword% %subdirectorySet1% %subdirectorySet2% %subdirectorySet3% %subdirectorySet4% %subdirectorySet5% -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/%projectCode%/%chargetoID%/%chargetoID%_%shipmentName%_%shipmentNumber%_zh-Hans.pkg
+
+REM zh/TW
+CALL ant -f C:\FileNet\Work\Current\IDCMS1\CPS\CloudOE\dita-translation-processing-master\handling_translated_files.xml -Dlang=zh/TW -DnoPrompt=true -DshipmentNumber=%shipmentNumber% -Dplugindir=%plugindir% -DcheckInComment="%checkInComment%" -Dgsa.userid=%gsaUserID% -Dgsa.password=%gsaUserPassword% %subdirectorySet1% %subdirectorySet2% %subdirectorySet3% %subdirectorySet4% %subdirectorySet5% -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/%projectCode%/%chargetoID%/%chargetoID%_%shipmentName%_%shipmentNumber%_zh-Hant.pkg
