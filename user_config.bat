fdb74f5faddee0207e181ba582109875da404358
Kristin A. Brown <kakronst@us.ibm.com>
2016-10-18 13:42:54 -0400

    Removed value

--- user_config.bat
+++ user_config.bat
@@ -16,4 +16,4 @@ SET githubPassword=
 
 REM Enter the directory path to the location where you downloaded this repo.
 REM SET installDir=C:\FileNet\Work\Current\IDCMS1\CPS\CloudOE\markdown_translation_processing-master
-SET installDir=C:\FileNet\Work\Current\IDCMS1\CPS\CloudOE\markdown_translation_processing-master
+SET installDir=
