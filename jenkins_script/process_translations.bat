754bb53f9ff5e2c97d3a3b09753da363014b5f40
Kristin A. Brown <kakronst@us.ibm.com>
2017-08-07 10:45:22 -0400

    Commented out git pull

--- jenkins_script/process_translations.bat
+++ jenkins_script/process_translations.bat
@@ -56,7 +56,7 @@ CALL ant -f handling_translated_files.xml -Dlang=zh/TW -DnoPrompt=true -Dshipmen
 CD %installDir%/%PluginNameShort%
 
 ECHO GIT pull
-GIT pull master
+#GIT pull
 
 ECHO GIT add --all
 GIT add --all
