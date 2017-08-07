ca462b670220ac21d8f8fd88db39826a151d98fe
Kristin A. Brown <kakronst@us.ibm.com>
2017-08-07 15:24:32 -0400

    Update process_translations.bat

--- jenkins_script/process_translations.bat
+++ jenkins_script/process_translations.bat
@@ -73,6 +73,6 @@ ECHO GIT commit
 GIT commit -m "%checkInComment%"
 
 ECHO GIT push
-GIT push
+GIT push origin
 REM git push --set-upstream https://github.com/IBM-Bluemix-Docs/schematics.git master
 REM git push https://alchemyDocs:Alchemy3Bluemix!@github.com/IBM-Bluemix-Docs/schematics.git --all
