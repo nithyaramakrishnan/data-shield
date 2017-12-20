1c81e7fc491dd4e9431cd0fe992166194aafe041
Kristin A. Brown <kakronst@us.ibm.com>
2017-12-20 11:41:57 -0500

    Added EnableDelayedExpansion

--- jenkins_script/process_translations.bat
+++ jenkins_script/process_translations.bat
@@ -1,63 +1,63 @@
-
+SETLOCAL EnableDelayedExpansion
 
 CD "C:\Program Files (x86)\Jenkins\jobs\environment-variables\services\build_services"
 
 FOR %%M in ("*.bat") DO (
 CALL %%M
-SET PluginNameShort=%GITHUB_REPO%
-SET localPluginDir=%SERVICE_OUTPUT_DIR%
-ECHO %PluginNameShort%
-ECHO %localPluginDir%
+SET PluginNameShort=!GITHUB_REPO!
+SET localPluginDir=!SERVICE_OUTPUT_DIR!
+ECHO !PluginNameShort!
+ECHO !localPluginDir!
 
 IF DEFINED CHARGEtoID (
 
-CD %installDir%
+CD !installDir!
 
-RD /S /Q %PluginNameShort%
+RD /S /Q !PluginNameShort!
 
-GIT clone https://%GITHUB_USERNAME%:%GITHUB_TOKEN%@github.com/IBM-Bluemix-Docs/%PluginNameShort%.git
+GIT clone https://!GITHUB_USERNAME!:!GITHUB_TOKEN!@github.com/IBM-Bluemix-Docs/!PluginNameShort!.git
 
 GIT init
-CD %PluginNameShort%
+CD !PluginNameShort!
 GIT init
 
 
-CD %installDir%
+CD !installDir!
 
 REM Run the script on each language.
 REM de
-CALL ant -f handling_translated_files.xml -Dlang=de -DmergeFiles=%mergeFiles% -DnoPrompt=true -DshipmentNumber=%shipmentNumber% -DlocalPluginDir=%localPluginDir% -DPluginNameShort=%PluginNameShort% -DinstallDir=%installDir% -DpackageExtension=%packageExtension% -DcheckInComment="%checkInComment%" -Dgsa.userid=%gsaUserID% -Dgsa.password=%gsaUserPassword% -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/%projectCode%/%chargetoID%/%chargetoID%_%shipmentName%_%shipmentNumber%_de%packageExtension% 
+CALL ant -f handling_translated_files.xml -Dlang=de -DmergeFiles=!mergeFiles! -DnoPrompt=true -DshipmentNumber=!shipmentNumber! -DlocalPluginDir=!localPluginDir! -DPluginNameShort=!PluginNameShort! -DinstallDir=!installDir! -DpackageExtension=!packageExtension! -DcheckInComment="!checkInComment!" -Dgsa.userid=!gsaUserID! -Dgsa.password=!gsaUserPassword! -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/!projectCode!/!chargetoID!/!chargetoID!_!shipmentName!_!shipmentNumber!_de!packageExtension! 
 
 REM es
-CALL ant -f handling_translated_files.xml -Dlang=es -DmergeFiles=%mergeFiles% -DnoPrompt=true -DshipmentNumber=%shipmentNumber% -DlocalPluginDir=%localPluginDir% -DPluginNameShort=%PluginNameShort% -DinstallDir=%installDir% -DpackageExtension=%packageExtension% -DcheckInComment="%checkInComment%" -Dgsa.userid=%gsaUserID% -Dgsa.password=%gsaUserPassword% -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/%projectCode%/%chargetoID%/%chargetoID%_%shipmentName%_%shipmentNumber%_es%packageExtension%
+CALL ant -f handling_translated_files.xml -Dlang=es -DmergeFiles=!mergeFiles! -DnoPrompt=true -DshipmentNumber=!shipmentNumber! -DlocalPluginDir=!localPluginDir! -DPluginNameShort=!PluginNameShort! -DinstallDir=!installDir! -DpackageExtension=!packageExtension! -DcheckInComment="!checkInComment!" -Dgsa.userid=!gsaUserID! -Dgsa.password=!gsaUserPassword! -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/!projectCode!/!chargetoID!/!chargetoID!_!shipmentName!_!shipmentNumber!_es!packageExtension!
 
 REM fr
-CALL ant -f handling_translated_files.xml -Dlang=fr -DmergeFiles=%mergeFiles% -DnoPrompt=true -DshipmentNumber=%shipmentNumber% -DlocalPluginDir=%localPluginDir% -DPluginNameShort=%PluginNameShort% -DinstallDir=%installDir% -DpackageExtension=%packageExtension% -DcheckInComment="%checkInComment%" -Dgsa.userid=%gsaUserID% -Dgsa.password=%gsaUserPassword% -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/%projectCode%/%chargetoID%/%chargetoID%_%shipmentName%_%shipmentNumber%_fr%packageExtension%
+CALL ant -f handling_translated_files.xml -Dlang=fr -DmergeFiles=!mergeFiles! -DnoPrompt=true -DshipmentNumber=!shipmentNumber! -DlocalPluginDir=!localPluginDir! -DPluginNameShort=!PluginNameShort! -DinstallDir=!installDir! -DpackageExtension=!packageExtension! -DcheckInComment="!checkInComment!" -Dgsa.userid=!gsaUserID! -Dgsa.password=!gsaUserPassword! -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/!projectCode!/!chargetoID!/!chargetoID!_!shipmentName!_!shipmentNumber!_fr!packageExtension!
 
 REM it	
-CALL ant -f handling_translated_files.xml -Dlang=it -DmergeFiles=%mergeFiles% -DnoPrompt=true -DshipmentNumber=%shipmentNumber% -DlocalPluginDir=%localPluginDir% -DPluginNameShort=%PluginNameShort% -DinstallDir=%installDir% -DpackageExtension=%packageExtension% -DcheckInComment="%checkInComment%" -Dgsa.userid=%gsaUserID% -Dgsa.password=%gsaUserPassword% -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/%projectCode%/%chargetoID%/%chargetoID%_%shipmentName%_%shipmentNumber%_it%packageExtension%
+CALL ant -f handling_translated_files.xml -Dlang=it -DmergeFiles=!mergeFiles! -DnoPrompt=true -DshipmentNumber=!shipmentNumber! -DlocalPluginDir=!localPluginDir! -DPluginNameShort=!PluginNameShort! -DinstallDir=!installDir! -DpackageExtension=!packageExtension! -DcheckInComment="!checkInComment!" -Dgsa.userid=!gsaUserID! -Dgsa.password=!gsaUserPassword! -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/!projectCode!/!chargetoID!/!chargetoID!_!shipmentName!_!shipmentNumber!_it!packageExtension!
 
 REM ja
-CALL ant -f handling_translated_files.xml -Dlang=ja -DmergeFiles=%mergeFiles% -DnoPrompt=true -DshipmentNumber=%shipmentNumber% -DlocalPluginDir=%localPluginDir% -DPluginNameShort=%PluginNameShort% -DinstallDir=%installDir% -DpackageExtension=%packageExtension% -DcheckInComment="%checkInComment%" -Dgsa.userid=%gsaUserID% -Dgsa.password=%gsaUserPassword% -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/%projectCode%/%chargetoID%/%chargetoID%_%shipmentName%_%shipmentNumber%_ja%packageExtension%
+CALL ant -f handling_translated_files.xml -Dlang=ja -DmergeFiles=!mergeFiles! -DnoPrompt=true -DshipmentNumber=!shipmentNumber! -DlocalPluginDir=!localPluginDir! -DPluginNameShort=!PluginNameShort! -DinstallDir=!installDir! -DpackageExtension=!packageExtension! -DcheckInComment="!checkInComment!" -Dgsa.userid=!gsaUserID! -Dgsa.password=!gsaUserPassword! -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/!projectCode!/!chargetoID!/!chargetoID!_!shipmentName!_!shipmentNumber!_ja!packageExtension!
 
 REM KO
-CALL ant -f handling_translated_files.xml -Dlang=ko -DmergeFiles=%mergeFiles% -DnoPrompt=true -DshipmentNumber=%shipmentNumber% -DlocalPluginDir=%localPluginDir% -DPluginNameShort=%PluginNameShort% -DinstallDir=%installDir% -DpackageExtension=%packageExtension% -DcheckInComment="%checkInComment%" -Dgsa.userid=%gsaUserID% -Dgsa.password=%gsaUserPassword% -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/%projectCode%/%chargetoID%/%chargetoID%_%shipmentName%_%shipmentNumber%_ko%packageExtension%
+CALL ant -f handling_translated_files.xml -Dlang=ko -DmergeFiles=!mergeFiles! -DnoPrompt=true -DshipmentNumber=!shipmentNumber! -DlocalPluginDir=!localPluginDir! -DPluginNameShort=!PluginNameShort! -DinstallDir=!installDir! -DpackageExtension=!packageExtension! -DcheckInComment="!checkInComment!" -Dgsa.userid=!gsaUserID! -Dgsa.password=!gsaUserPassword! -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/!projectCode!/!chargetoID!/!chargetoID!_!shipmentName!_!shipmentNumber!_ko!packageExtension!
 
 REM PT/BR
-CALL ant -f handling_translated_files.xml -Dlang=pt/BR -DmergeFiles=%mergeFiles% -DnoPrompt=true -DshipmentNumber=%shipmentNumber% -DlocalPluginDir=%localPluginDir% -DPluginNameShort=%PluginNameShort% -DinstallDir=%installDir% -DpackageExtension=%packageExtension% -DcheckInComment="%checkInComment%" -Dgsa.userid=%gsaUserID% -Dgsa.password=%gsaUserPassword% -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/%projectCode%/%chargetoID%/%chargetoID%_%shipmentName%_%shipmentNumber%_pt-BR%packageExtension%
+CALL ant -f handling_translated_files.xml -Dlang=pt/BR -DmergeFiles=!mergeFiles! -DnoPrompt=true -DshipmentNumber=!shipmentNumber! -DlocalPluginDir=!localPluginDir! -DPluginNameShort=!PluginNameShort! -DinstallDir=!installDir! -DpackageExtension=!packageExtension! -DcheckInComment="!checkInComment!" -Dgsa.userid=!gsaUserID! -Dgsa.password=!gsaUserPassword! -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/!projectCode!/!chargetoID!/!chargetoID!_!shipmentName!_!shipmentNumber!_pt-BR!packageExtension!
 
 REM zh/CN
-CALL ant -f handling_translated_files.xml -Dlang=zh/CN -DmergeFiles=%mergeFiles% -DnoPrompt=true -DshipmentNumber=%shipmentNumber% -DlocalPluginDir=%localPluginDir% -DPluginNameShort=%PluginNameShort% -DinstallDir=%installDir% -DpackageExtension=%packageExtension% -DcheckInComment="%checkInComment%" -Dgsa.userid=%gsaUserID% -Dgsa.password=%gsaUserPassword% -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/%projectCode%/%chargetoID%/%chargetoID%_%shipmentName%_%shipmentNumber%_zh-Hans%packageExtension%
+CALL ant -f handling_translated_files.xml -Dlang=zh/CN -DmergeFiles=!mergeFiles! -DnoPrompt=true -DshipmentNumber=!shipmentNumber! -DlocalPluginDir=!localPluginDir! -DPluginNameShort=!PluginNameShort! -DinstallDir=!installDir! -DpackageExtension=!packageExtension! -DcheckInComment="!checkInComment!" -Dgsa.userid=!gsaUserID! -Dgsa.password=!gsaUserPassword! -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/!projectCode!/!chargetoID!/!chargetoID!_!shipmentName!_!shipmentNumber!_zh-Hans!packageExtension!
 
 REM zh/TW
-CALL ant -f handling_translated_files.xml -Dlang=zh/TW -DmergeFiles=%mergeFiles% -DnoPrompt=true -DshipmentNumber=%shipmentNumber% -DlocalPluginDir=%localPluginDir% -DPluginNameShort=%PluginNameShort% -DinstallDir=%installDir% -DpackageExtension=%packageExtension% -DcheckInComment="%checkInComment%" -Dgsa.userid=%gsaUserID% -Dgsa.password=%gsaUserPassword% -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/%projectCode%/%chargetoID%/%chargetoID%_%shipmentName%_%shipmentNumber%_zh-Hant%packageExtension%
+CALL ant -f handling_translated_files.xml -Dlang=zh/TW -DmergeFiles=!mergeFiles! -DnoPrompt=true -DshipmentNumber=!shipmentNumber! -DlocalPluginDir=!localPluginDir! -DPluginNameShort=!PluginNameShort! -DinstallDir=!installDir! -DpackageExtension=!packageExtension! -DcheckInComment="!checkInComment!" -Dgsa.userid=!gsaUserID! -Dgsa.password=!gsaUserPassword! -Dpkg.url=https://rtpgsa.ibm.com/projects/c/cfm/CentralNLV/!projectCode!/!chargetoID!/!chargetoID!_!shipmentName!_!shipmentNumber!_zh-Hant!packageExtension!
 
 
-CD %installDir%/%PluginNameShort%
+CD !installDir!/!PluginNameShort!
 
 
 ECHO GIT pull
-GIT pull https://%GITHUB_USERNAME%:%GITHUB_TOKEN%@github.com/IBM-Bluemix-Docs/%PluginNameShort%.git
+GIT pull https://!GITHUB_USERNAME!:!GITHUB_TOKEN!@github.com/IBM-Bluemix-Docs/!PluginNameShort!.git
 
 ECHO GIT add --all
 GIT add --all
@@ -66,17 +66,19 @@ ECHO GIT status
 GIT status
 
 REM ECHO GIT commit
-REM GIT commit -m "%checkInComment%"
+REM GIT commit -m "!checkInComment!"
 
 REM ECHO GIT merge
 REM GIT merge
 
 REM ECHO GIT remote add
-REM GIT remote add %PluginNameShort% https://github.com/IBM-Bluemix-Docs/%PluginNameShort%.git
+REM GIT remote add !PluginNameShort! https://github.com/IBM-Bluemix-Docs/!PluginNameShort!.git
 
-REM ECHO GIT push %PluginNameShort%
-REM GIT push %PluginNameShort%
+REM ECHO GIT push !PluginNameShort!
+REM GIT push !PluginNameShort!
 
 SET CHARGEtoID=
 )
 )
+
+ENDLOCAL
