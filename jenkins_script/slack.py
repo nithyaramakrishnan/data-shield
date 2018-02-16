dd810c01dde99156b7b070980f6722e16434103e
Kristin A. Brown <kakronst@us.ibm.com>
2018-02-16 09:55:03 -0500

    Condensed prints

--- jenkins_script/slack.py
+++ jenkins_script/slack.py
@@ -10,19 +10,12 @@ SLACK_ICON = os.environ.get('SLACK_ICON')  # Slack Bot icon
 SLACK_INCOMING_WEB_HOOK = "https://hooks.slack.com/services/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"
 SLACK_INCOMING_CHANNEL = "#doctopus-translation"  # Slack Channel
 
-print "Service:"
-print Service
-print "GITHUB_REPO:"
-print GITHUB_REPO
-print "SLACK_INCOMING_USER:"
-print SLACK_INCOMING_USER
-print "SLACK_ICON:"
-print SLACK_ICON
-print "SLACK_INCOMING_WEB_HOOK:"
-print SLACK_INCOMING_WEB_HOOK
-print "SLACK_INCOMING_CHANNEL:"
-print SLACK_INCOMING_CHANNEL
-
+print "Service:" + Service
+print "GITHUB_REPO: " + GITHUB_REPO
+print "SLACK_INCOMING_USER: " + SLACK_INCOMING_USER
+print "SLACK_ICON: " + SLACK_ICON
+print "SLACK_INCOMING_WEB_HOOK: " + SLACK_INCOMING_WEB_HOOK
+print "SLACK_INCOMING_CHANNEL: " + SLACK_INCOMING_CHANNEL
 
 payload = {"channel":SLACK_INCOMING_CHANNEL,"username":SLACK_INCOMING_USER,"text":"The translation return build is complete for " + Service + "! Verify the returns in the prod repo https://github.com/IBM-Bluemix-Docs/" + GITHUB_REPO + "/tree/master/nl.","icon_emoji":SLACK_ICON}
 print payload
