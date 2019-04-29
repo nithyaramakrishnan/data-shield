a3e6bc2394db5edbdc8d557c7293d0f37d476b23
Kristin A. Brown <kakronst@us.ibm.com>
2019-04-29 09:37:40 -0400

    Added pkg url to error msg

--- hosted/doctopus.sh
+++ hosted/doctopus.sh
@@ -132,7 +132,7 @@ do
 						summary="$summary\\n$LANG_SUCCESS $lang"
 					else
 						echo "Package could not be unzipped. The package might not have been returned yet or it might be corrupt. $lang check-in was not completed."
-						summary="$summary\\n$LANG_FAIL $lang: Package could not be unzipped. The package might not have been returned yet or it might be corrupt. $lang check-in was not completed."
+						summary="$summary\\n$LANG_FAIL $lang: Package could not be unzipped. The package might not have been returned yet or it might be corrupt. $lang check-in was not completed: $pkgURL"
 					fi
 				else
 					echo "Package could not be renamed. $lang check-in was not completed."
