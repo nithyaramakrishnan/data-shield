c0459be475eab83a7d74b74ac818af39b861ae4d
Kristin A. Brown <kakronst@us.ibm.com>
2019-05-01 11:41:11 -0400

    Update doctopus.sh

--- hosted/doctopus.sh
+++ hosted/doctopus.sh
@@ -188,8 +188,12 @@ do
 		export fatal=fatal
 		echo $pushResult
 		echo $fatal
-		s=${pushResult//$fatal}
-		count="$(((${#pushResult} - ${#s}) / ${#fatal}))"
+		removeEndPushResult=${pushResult%fatal*}
+		echo "removeEndPushResult: $removeEndPushResult"
+		removeBeginningPushResult=${removeEndPushResult#*fatal}
+		echo "removeBeginningPushResult: $removeBeginningPushResult"
+		#s=${pushResult//$fatal}
+		count="$(((${#pushResult} - ${#removeEndPushResult} - ${#removeBeginningPushResult}) / ${#fatal}))"
 		if [ $count > 0 ] ;then
 			summary=":failed-6474:  The commit to the Github repository failed with the following error: \n$pushResult"
 		fi
