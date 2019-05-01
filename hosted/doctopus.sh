6e833e32677365dc7962a73ec9067b533bb1fabe
Kristin A. Brown <kakronst@us.ibm.com>
2019-05-01 09:31:25 -0400

    Update doctopus.sh

--- hosted/doctopus.sh
+++ hosted/doctopus.sh
@@ -186,7 +186,8 @@ do
 		echo git push translations
 		pushResult=$(git push translations 2>&1)
 		echo "Push result: $pushResult \n"
-		export fatal="fatal"
+		export fatal=fatal
+		echo $fatal
 		if [ "$pushResult" = *"$fatal"* ] ; then
 			summary=":failed-6474:  The commit to the Github repository failed with the following error: \n$pushResult"
 		fi
@@ -325,7 +326,7 @@ do
 			echo git push translations-cli
 			pushResult=$(git push translations-cli  2>&1)
 			echo "Push result: $pushResult\n"
-			export fatal="fatal"
+			export fatal=fatal
 			echo $fatal
 			if [ "$pushResult" = *"$fatal"* ] ; then
 				summary=":failed-6474:  The commit to the Github repository failed with the following error: \n$pushResult"
